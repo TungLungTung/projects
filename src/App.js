b7fcfea0380882ad90d0b25101169686aa5c00e7
Le Thanh Tung <tunglungtung87@gmail.com>
2021-10-06 14:06:29 +0700

    update redirect user when login

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import {connect} from 'react-redux'
 import HomePage from './pages/homepage/homepage.component';
 import NotFound404 from './pages/_404/_404.component';
 import ShopPage from './pages/shop/shop.components'
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, Redirect } from 'react-router-dom'
 import Header from './components/layouts/header/header.component';
 import SignInAndSignUpPage from './pages/account/auth/sign-in-and-sign-up.component';
 
@@ -75,7 +75,7 @@ class App extends React.Component {
           {/* Exact la luc nao cung phai co o trang chu */}
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SignInAndSignUpPage} />
+          <Route exact path='/signin' render={(props) => this.props.currentUser ? (<Redirect to='/' />) : (<SignInAndSignUpPage />)} />
           <Route path="*">
             <NotFound404 />
           </Route>
@@ -87,12 +87,16 @@ class App extends React.Component {
 
 }
 
+const mapStateToProps = ({user}) => ({
+  currentUser: user.currentUser,
+})
+
 const mapDispatchProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchProps
 )(App);
--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,3 +1,5 @@
+import { UserActionTypes } from "./user.types";
+
 // Nay giong nhu this.state ben trong constructor()
 const INITIAL_STATE = {
     currentUser: null
@@ -5,7 +7,7 @@ const INITIAL_STATE = {
 
 const userReducer = (state = INITIAL_STATE, action) => {
     switch(action.type) {
-        case 'SET_CURRENT_USER':
+        case UserActionTypes.SET_CURRENT_USER:
             return {
                 ...state,
                 currentUser: action.payload
